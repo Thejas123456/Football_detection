0bba060e55e8616d796e319403ec28c81ceec23e
IOTA <33376412+Thejas123456@users.noreply.github.com>
2023-04-27 20:12:21 +0530

    Update train.py

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 import os
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
-model = YOLO("yolov8s.yaml")  # build a new model from scratch
+model = YOLO("yolov8s.yaml")  
 model =YOLO("last.pt")
 if __name__ == '__main__':
-    model.train(data="data.yaml", epochs=400,batch =8)  # train the model
+    model.train(data="data.yaml", epochs=400,batch =8)  
